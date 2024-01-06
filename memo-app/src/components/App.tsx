1819d3f7572b2fc573fa0ab665e1233888359ac3
ShinHeeJin <sisesu92@gmail.com>
2024-01-06 22:46:43 +0900

    메모 애플리케이션

--- memo-app/src/components/App.tsx
+++ memo-app/src/components/App.tsx
@@ -1,3 +1,61 @@
-export const App = () => {
-  return <h1>간단한 메모 애플리케이션</h1>;
+import { ChangeEvent, FC, useState } from "react";
+import { styled } from "styled-components";
+
+export const App: FC = () => {
+  const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const onChangeText = function (event: ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    setText("");
+  };
+
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+    setText("");
+  };
+
+  return (
+    <div>
+      <h1>간단한 메모 애플리케이션</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>추가</SButton>
+      <SContainer>
+        <p>메모 목록</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={index}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>삭제 </SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
+    </div>
+  );
 };
+
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
