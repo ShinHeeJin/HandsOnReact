1ace22afb7e7b8b4113729085af205f31fcd3762
ShinHeeJin <sisesu92@gmail.com>
2024-01-07 15:14:59 +0900

    ✏️ 댓글 리스트 컴포넌트

--- 3. 소플의 처음만난 리액트/src/src/chapter_05/CommentList.jsx
+++ 3. 소플의 처음만난 리액트/src/src/chapter_05/CommentList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+  {
+    name: "홍길동",
+    comment: "안녕하세요!",
+  },
+  {
+    name: "고길동",
+    comment: "반갑습니다!",
+  },
+];
+
 function CommentList(props) {
   return (
     <div>
-      <Comment />
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
   );
 }
