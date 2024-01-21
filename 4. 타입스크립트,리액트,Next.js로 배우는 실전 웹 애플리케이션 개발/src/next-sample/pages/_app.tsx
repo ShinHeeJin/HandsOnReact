0a465ec606731d162e6ae636ea8f88d47b8a46d7
ShinHeeJin <sisesu92@gmail.com>
2024-01-22 01:53:07 +0900

    📝 theme 활용하기

---
+++ 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/next-sample/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  // styled-conponents로 테마를 사용하기 위해 ThemeProvider를 둔다
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
