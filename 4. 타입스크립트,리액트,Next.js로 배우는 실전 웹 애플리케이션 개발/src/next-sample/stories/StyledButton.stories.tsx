977191408ea51635615d4f5d46c6854a6181bbc5
ShinHeeJin <sisesu92@gmail.com>
2024-01-25 23:42:53 +0900

    ✏️ StoryBook 기본 및 액션 콜백 핸들링

---
+++ 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/next-sample/stories/StyledButton.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentMeta } from "@storybook/react";
+import { StyledButton } from "../components/StyledButton";
+import { action } from "@storybook/addon-actions";
+import { useState } from "react";
+
+export default {
+  title: "StyledButton (1)",
+  component: StyledButton,
+  argTypes: { onClick: { action: "clicked" } },
+} as ComponentMeta<typeof StyledButton>;
+
+const incrementAction = action("increment");
+
+export const Primary = (props) => {
+  const [count, setCount] = useState(0);
+
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count);
+    setCount((c) => c + 1);
+  };
+  return (
+    <StyledButton {...props} variant="primary" onClick={onClick}>
+      Primary
+    </StyledButton>
+  );
+};
+
+export const Success = (props) => {
+  return (
+    <StyledButton {...props} variant="success">
+      Primary
+    </StyledButton>
+  );
+};
+
+export const Transparent = (props) => {
+  return (
+    <StyledButton {...props} variant="transparent">
+      Transparent
+    </StyledButton>
+  );
+};
