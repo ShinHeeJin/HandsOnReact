bd0f6c5a0fe497ce3836d90fe0ab40add14e8eb4
ShinHeeJin <sisesu92@gmail.com>
2024-01-28 01:36:09 +0900

    ✏️ Input 엘리먼트 테스트 케이스 2건 추가

--- 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/next-sample/components/Input/index.spec.tsx
+++ 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/next-sample/components/Input/index.spec.tsx
@@ -1,4 +1,9 @@
-import { render, screen, RenderResult } from "@testing-library/react";
+import {
+  render,
+  screen,
+  RenderResult,
+  fireEvent,
+} from "@testing-library/react";
 import { Input } from "./index";
 
 // describe로 처리를 모은다.
@@ -16,6 +21,29 @@ describe("Input", () => {
   it("should empty in input on initial render", () => {
     const inputNode = screen.getByLabelText("Username") as HTMLInputElement;
 
+    expect(inputNode).toHaveValue("");
+  });
+
+  // 문자를 입력했을 때, 입력한 내용이 표시되는지 테스트
+  it("should show input text", () => {
+    const inputText = "Test Input Text";
+    const inputNode = screen.getByLabelText("Username") as HTMLInputElement;
+
+    fireEvent.change(inputNode, { target: { value: inputText } });
+    expect(inputNode).toHaveValue(inputText);
+  });
+
+  // 버튼이 클릭되면 입력 텍스트가 클리어하는지 체크
+  it("should reset when user clicks button", () => {
+    const inputText = "Test Input Text";
+    const inputNode = screen.getByLabelText("Username") as HTMLInputElement;
+    fireEvent.change(inputNode, { target: { value: inputText } });
+
+    const buttonNode = screen.getByRole("button", {
+      name: "Reset",
+    }) as HTMLButtonElement;
+
+    fireEvent.click(buttonNode);
     expect(inputNode).toHaveValue("");
   });
 });
