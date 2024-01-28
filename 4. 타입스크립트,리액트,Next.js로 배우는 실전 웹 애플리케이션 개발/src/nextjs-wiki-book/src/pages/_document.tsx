7233c10be4493f55eb0b22d3948c0cdcbd6b02d0
ShinHeeJin <sisesu92@gmail.com>
2024-01-28 23:11:40 +0900

    ✏️ 프로젝트 세팅
    - next.config.mjs 수정
    - _document.tsx 수정
    - _app.tsx 작성

--- 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/nextjs-wiki-book/src/pages/_document.tsx
+++ 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/nextjs-wiki-book/src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// 기본 Document를 MyDocument로 덮어 쓴다
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      // 초깃값을 유용한다
+      const initialProps = await Document.getInitialProps(ctx);
+
+      // initialProps에 더해, style을 추가해서 반환한다.
+      return {
+        ...initialProps,
+        styles: [
+          // 원래 style
+          initialProps.styles,
+          // styled-components의 style
+          sheet.getStyleElement(),
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
