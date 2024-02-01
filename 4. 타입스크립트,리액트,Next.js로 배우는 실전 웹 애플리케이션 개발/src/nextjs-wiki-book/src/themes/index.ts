0db48a57c2d1e7e8deb54fdcf21479ca43c8c66f
ShinHeeJin <sisesu92@gmail.com>
2024-02-01 23:32:09 +0900

    ✏️ 테마설정 작성

---
+++ 4. 타입스크립트,리액트,Next.js로 배우는 실전 웹 애플리케이션 개발/src/nextjs-wiki-book/src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from "./colors";
+import fonstSizes from "./fontSizes";
+import letterSpacings from "./letterSpacings";
+import lineHeights from "./lineHeights";
+import space from "./space";
+
+export const theme = {
+  space,
+  fonstSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const;
